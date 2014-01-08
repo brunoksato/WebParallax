cca3d214038a034bb7ff362600609c1c0c5577f5
Bruno Sato <bruno.sato@live.com>
2014-01-08 17:15:08 -0200

    scroll modify

--- js/script.js
+++ js/script.js
@@ -77,36 +77,41 @@ $(document).ready(function(){
 
 }); // document ready
 
-$('nav ul li a').on('click', function () {
-    var go = $(this).attr('href');
-    if (go == '#first') {
+$(function(){
+    $('nav ul li a').on('click', function () {
+        var go = $(this).attr('href');
+        if (go == '#first') {
             var getPosition = $('#first').offset().top;
             $('html,body').animate({ scrollTop: getPosition }, 'slow');
             return false;
-    }
-    else if (go == '#second') {
+        }
+        else if (go == '#second') {
             var getPosition = $('#second').offset().top - 60;
             $('html,body').animate({ scrollTop: getPosition }, 'slow');
             return false;
-    }
-    else if (go == '#third') {
+        }
+        else if (go == '#third') {
             var getPosition = $('#third').offset().top - 10;
             $('html,body').animate({ scrollTop: getPosition - 50 }, 'slow');
             return false;
-    }
-    else if (go == '#fourth') {
+        }
+        else if (go == '#fourth') {
             var getPosition = $('#fourth').offset().top - 60;
             $('html,body').animate({ scrollTop: getPosition }, 'slow');
             return false;
-    }
-    else if (go == '#theend') {
+        }
+        else if (go == '#theend') {
             var getPosition = $('#theend').offset().top -60;
             $('html,body').animate({ scrollTop: getPosition }, 'slow');
             return false;
-    }
-    else if (go == '#five') {
+        }
+        else if (go == '#five') {
             var getPosition = $('#five').offset().top -60;
             $('html,body').animate({ scrollTop: getPosition }, 'slow');
             return false;
-    }
+        }
+    });
 });
+
+
+
